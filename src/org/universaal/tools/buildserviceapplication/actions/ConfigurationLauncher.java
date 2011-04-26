b91425a74bf73e781a31453d0d43e593abee4b7f
Konstantinos Giannoutakis <kgiannou@iti.gr>
2011-04-26 11:00:26 +0000

    latest version

--- src/org/universaal/tools/buildserviceapplication/actions/ConfigurationLauncher.java
+++ src/org/universaal/tools/buildserviceapplication/actions/ConfigurationLauncher.java
@@ -1,43 +1,19 @@
 package org.universaal.tools.buildserviceapplication.actions;
 
 
-import java.io.File;
-import java.text.Collator;
-import java.util.*;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.*;
 import org.eclipse.debug.core.*;
-import org.eclipse.debug.ui.DebugUITools;
-import org.eclipse.debug.ui.IDebugModelPresentation;
-import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.window.Window;
-import org.eclipse.pde.core.plugin.IPluginModelBase;
-import org.eclipse.pde.core.plugin.PluginRegistry;
-import org.eclipse.pde.internal.core.*;
-import org.eclipse.pde.internal.core.ifeature.*;
-import org.eclipse.pde.internal.core.iproduct.*;
-import org.eclipse.pde.internal.core.iproduct.IProduct;
-import org.eclipse.pde.internal.core.util.CoreUtility;
-import org.eclipse.pde.internal.ui.*;
-import org.eclipse.pde.internal.ui.editor.PDELauncherFormEditor;
-import org.eclipse.pde.ui.launcher.*;
-import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+
 
 public class ConfigurationLauncher extends Action{
-	private IProduct fProduct;
-	private String fMode;
-	private String fPath;
 	private String configurationName="";
 
-	public ConfigurationLauncher(IProduct product, String path, String mode) {
-		fProduct = product;
-		fMode = mode;
-		fPath = path;
-	}
+	
 	public ConfigurationLauncher(String configurationName) {
 		this.configurationName=configurationName;
 		
@@ -48,6 +24,14 @@ public class ConfigurationLauncher extends Action{
 			ILaunchConfiguration config = findLaunchConfiguration();			
 			if (config != null){
 				config.launch(ILaunchManager.RUN_MODE, null);			
+			}
+			else{
+				ResourcesPlugin.getWorkspace().getRoot()
+				.refreshLocal(IResource.DEPTH_INFINITE, null);
+				MessageDialog
+				.openInformation(null,
+						"BuildServiceApplication",
+						"An error occured while running service.\n Please try again");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -75,7 +59,7 @@ public class ConfigurationLauncher extends Action{
 		   ILaunchConfiguration[] lcs = manager.getLaunchConfigurations(type);
 		   for (int i = 0; i < lcs.length; ++i) {
 		        if (lcs[i].getName().equals(configurationName)) {
-		        	ILaunchConfigurationWorkingCopy t=lcs[i].getWorkingCopy();	
+		        	ILaunchConfiguration t=lcs[i];	
 		        	return t;		        	
 		        }
 		   }
--- src/org/universaal/tools/buildserviceapplication/actions/CreateConfigurationFile.java
+++ src/org/universaal/tools/buildserviceapplication/actions/CreateConfigurationFile.java
@@ -1,103 +1,90 @@
 package org.universaal.tools.buildserviceapplication.actions;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.InputStreamReader;
-
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.viewers.TreePath;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
 
 public class CreateConfigurationFile {
 
-	private String groupId = "";
+	static public String groupId = "";
 	static public String artifactId = "";
+	static public String artifactVersion = "";
 
 	public CreateConfigurationFile() {
 		getBundleProperties();
 	}
 
 	private void getBundleProperties() {
-		try {
-			FileInputStream fstream = new FileInputStream(
-					getSelectedProjectPath() + "/pom.xml");
-			DataInputStream in = new DataInputStream(fstream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String strLine;
-			boolean groupFound = false;
-			boolean artifactFound = false;
-			while ((strLine = br.readLine()) != null) {
-				if (strLine.contains("<groupId>")
-						&& strLine.contains("</groupId>")) {
-					groupId = strLine.split(">")[1].split("<")[0];
-					groupFound = true;
-					if (artifactFound) {
-						return;
-					}
+		try {			
+			Reader reader = new FileReader(BuildAction.getSelectedProjectPath()
+					+ "/pom.xml");			
+				MavenXpp3Reader xpp3Reader = new MavenXpp3Reader();
+				Model model = xpp3Reader.read(reader);
+				groupId=model.getGroupId();
+				artifactId=model.getArtifactId();
+				artifactVersion=model.getVersion();
+				
+				//if groupId is null then search within its parent
+				if(groupId==null&&model.getParent()!=null){
+					groupId=model.getParent().getGroupId();
 				}
-				if (strLine.contains("<artifactId>")
-						&& strLine.contains("</artifactId>")) {
-					artifactId = strLine.split(">")[1].split("<")[0];
-					artifactFound = true;
-					if (groupFound) {
-						return;
-					}
+				//if artifactId is null then search within its parent
+				if(artifactId==null&&model.getParent()!=null){
+					artifactId=model.getParent().getArtifactId();
 				}
-			}
-			in.close();
+				reader.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * Returns the root path of the selected project within eclipse workspace.
-	 */
-	private String getSelectedProjectPath() {
-
-		try {
-			String projectPath = "";
-			IWorkbench workbench = PlatformUI.getWorkbench();
-			IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-			TreeSelection selection = (TreeSelection) window.getActivePage()
-					.getSelection();
-			if (selection.getPaths().length != 0) {
-				TreePath path = selection.getPaths()[0];
-				Object sel = path.getSegment(0);
-				if (sel instanceof org.eclipse.core.internal.resources.Project) {
-					org.eclipse.core.internal.resources.Project selectedProject = (org.eclipse.core.internal.resources.Project) sel;
-					projectPath = selectedProject.getLocation().toString();
-				}
-				projectPath = projectPath.replace("file:/", "");
-				// System.out.println("Path to project: "+projectPath);
-				return projectPath;
-			} else {
-				return "";
-			}
-		} catch (Exception ex) {
-			return "";
-		}
-	}
 
 	public boolean createFile() {
+		List<Dependency> ontologyDependencies = new ArrayList<Dependency>();
+		try {
+			// Path to your local Maven repository
+			Reader reader = new FileReader(BuildAction.getSelectedProjectPath()
+					+ "/pom.xml");
+			try {
+				MavenXpp3Reader xpp3Reader = new MavenXpp3Reader();
+				Model model = xpp3Reader.read(reader);
+				List<Dependency> dependencies = model.getDependencies();
+				System.out.println("Project dependencies:\n");
+				for (int i = 0; i < dependencies.size(); i++) {
+					Dependency dependency = dependencies.get(i);
+					if (dependency.getGroupId().equals(
+							"org.universAAL.ontology")) {
+						ontologyDependencies.add(dependency);
+					}
+				}
+				reader.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				reader.close();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 		String pathToAddConf = Platform.getLocation()
 				+ "\\.metadata\\.plugins\\org.eclipse.debug.core\\.launches\\";
-		File dir=new File(pathToAddConf);
+		File dir = new File(pathToAddConf);
 		dir.mkdirs();
-		File launchFile=new File(pathToAddConf+artifactId+".launch");
-		if(launchFile.exists()){
+		File launchFile = new File(pathToAddConf + artifactId + ".launch");
+		if (launchFile.exists()) {
 			launchFile.delete();
 		}
 		try {
-			FileWriter fstream = new FileWriter(pathToAddConf
-					+ artifactId+".launch");
+			FileWriter fstream = new FileWriter(pathToAddConf + artifactId
+					+ ".launch");
 			BufferedWriter out = new BufferedWriter(fstream);
 			out.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
 			out.write("<launchConfiguration type=\"org.eclipse.pde.ui.EquinoxLauncher\">\n");
@@ -107,7 +94,8 @@ public class CreateConfigurationFile {
 			out.write("<stringAttribute key=\"bootstrap\" value=\"\"/>\n");
 			out.write("<stringAttribute key=\"checked\" value=\"[NONE]\"/>\n");
 			out.write("<booleanAttribute key=\"clearConfig\" value=\"false\"/>\n");
-			out.write("<stringAttribute key=\"configLocation\" value=\"${workspace_loc}/rundir/."+artifactId+"\"/>\n");
+			out.write("<stringAttribute key=\"configLocation\" value=\"${workspace_loc}/rundir/"
+							+ artifactId + "\"/>\n");
 			out.write("<booleanAttribute key=\"default_auto_start\" value=\"true\"/>\n");
 			out.write("<intAttribute key=\"default_start_level\" value=\"8\"/>\n");
 			out.write("<booleanAttribute key=\"includeOptional\" value=\"true\"/>\n");
@@ -117,7 +105,8 @@ public class CreateConfigurationFile {
 			out.write("<stringAttribute key=\"org.eclipse.jdt.launching.PROGRAM_ARGUMENTS\" value=\"-console --obrRepositories=http://a1gforge.igd.fraunhofer.de/nexus/content/repositories/snapshots/repository.xml,http://a1gforge.igd.fraunhofer.de/nexus/content/repositories/releases/repository.xml,http://bundles.osgi.org/obr/browse?_xml=1&amp;amp;cmd=repository --org.ops4j.pax.url.mvn.repositories=+http://a1gforge.igd.fraunhofer.de/nexus/content/groups/public,http://a1gforge.igd.fraunhofer.de/nexus/content/repositories/snapshots@snapshots@noreleases --log=DEBUG\"/>\n");
 			out.write("<stringAttribute key=\"org.eclipse.jdt.launching.SOURCE_PATH_PROVIDER\" value=\"org.eclipse.pde.ui.workbenchClasspathProvider\"/>\n");
 			out.write("<stringAttribute key=\"org.eclipse.jdt.launching.VM_ARGUMENTS\" value=\"-Dosgi.noShutdown=true -Dfelix.log.level=4 -Dorg.universAAL.middleware.peer.is_coordinator=true -Dorg.universAAL.middleware.peer.member_of=urn:org.universAAL.aal_space:test_env -Dbundles.configuration.location=${workspace_loc}/rundir/confadmin\"/>\n");
-			out.write("<stringAttribute key=\"org.eclipse.jdt.launching.WORKING_DIRECTORY\" value=\"${workspace_loc}/rundir/smp."+artifactId+"\"/>\n");
+			out.write("<stringAttribute key=\"org.eclipse.jdt.launching.WORKING_DIRECTORY\" value=\"${workspace_loc}/rundir/"
+							+ artifactId + "\"/>\n");
 			out.write("<stringAttribute key=\"org.ops4j.pax.cursor.logLevel\" value=\"DEBUG\"/>\n");
 			out.write("<booleanAttribute key=\"org.ops4j.pax.cursor.overwrite\" value=\"false\"/>\n");
 			out.write("<booleanAttribute key=\"org.ops4j.pax.cursor.overwriteSystemBundles\" value=\"false\"/>\n");
@@ -128,13 +117,20 @@ public class CreateConfigurationFile {
 			out.write("<mapAttribute key=\"org.ops4j.pax.cursor.provisionItems\">\n");
 			out.write("<mapEntry key=\"mvn:org.universAAL.middleware/mw.data.serialization\" value=\"true@true@4@false\"/>\n");
 			out.write("<mapEntry key=\"mvn:org.apache.felix/org.apache.felix.configadmin/1.2.4\" value=\"true@true@2@false\"/>\n");
-			//out.write("<mapEntry key=\"mvn:org.universAAL.ontology/ont.phWorld\" value=\"true@true@5@false\"/>\n");
+			for (int i = 0; i < ontologyDependencies.size(); i++) {
+				out.write("<mapEntry key=\"mvn:"
+						+ ontologyDependencies.get(i).getGroupId() + "/"
+						+ ontologyDependencies.get(i).getArtifactId()
+						+ "\" value=\"true@true@5@false\"/>\n");
+			}
+			
 			out.write("<mapEntry key=\"mvn:org.universAAL.middleware/mw.acl.interfaces\" value=\"true@true@2@false\"/>\n");
 			out.write("<mapEntry key=\"wrap:mvn:jp.go.ipa/jgcl/1.0\" value=\"true@true@2@false\"/>\n");
-			out.write("<mapEntry key=\"mvn:"+groupId+"/"+artifactId+"\" value=\"true@false@6@true\"/>\n");
+			out.write("<mapEntry key=\"mvn:" + groupId + "/" + artifactId
+					+ "\" value=\"true@false@6@true\"/>\n");
 			out.write("<mapEntry key=\"mvn:org.universAAL.middleware/mw.bus.model\" value=\"true@true@3@false\"/>\n");
 			out.write("<mapEntry key=\"mvn:org.universAAL.middleware/mw.bus.context\" value=\"true@true@4@false\"/>\n");
-			//out.write("<mapEntry key=\"mvn:org.universAAL.ontology/ont.weather\" value=\"true@true@5@true\"/>\n");
+			
 			out.write("<mapEntry key=\"mvn:org.universAAL.middleware/mw.data.representation\" value=\"true@true@4@false\"/>\n");
 			out.write("<mapEntry key=\"wrap:mvn:org.osgi/osgi_R4_compendium/1.0\" value=\"true@true@2@false\"/>\n");
 			out.write("<mapEntry key=\"mvn:org.universAAL.middleware/mw.bus.service\" value=\"true@true@4@false\"/>\n");
@@ -151,7 +147,13 @@ public class CreateConfigurationFile {
 			out.write("<listEntry value=\"--overwriteSystemBundles=false\"/>\n");
 			out.write("<listEntry value=\"--log=DEBUG\"/>\n");
 			out.write("<listEntry value=\"--profiles=obr\"/>\n");
-		//	out.write("<listEntry value=\"mvn:org.universAAL.ontology/ont.phWorld@5\"/>\n");
+			for (int i = 0; i < ontologyDependencies.size(); i++) {
+				out.write("<listEntry value=\"mvn:"
+						+ ontologyDependencies.get(i).getGroupId() + "/"
+						+ ontologyDependencies.get(i).getArtifactId()
+						+ "@5\"/>\n");
+			}
+			
 			out.write("<listEntry value=\"mvn:org.apache.felix/org.apache.felix.configadmin/1.2.4@2\"/>\n");
 			out.write("<listEntry value=\"mvn:org.universAAL.middleware/mw.acl.interfaces@2\"/>\n");
 			out.write("<listEntry value=\"wrap:mvn:jp.go.ipa/jgcl/1.0@2\"/>\n");
@@ -163,10 +165,11 @@ public class CreateConfigurationFile {
 			out.write("<listEntry value=\"wrap:mvn:org.ops4j.pax.logging/pax-logging-service/1.4@3\"/>\n");
 			out.write("<listEntry value=\"wrap:mvn:java3d/j3d-core/1.3.1@2\"/>\n");
 			out.write("<listEntry value=\"mvn:org.universAAL.middleware/mw.data.serialization@4\"/>\n");
-			out.write("<listEntry value=\"mvn:"+groupId+"/"+artifactId+"@6@nostart@update\"/>\n");
+			out.write("<listEntry value=\"mvn:" + groupId + "/" + artifactId
+					+ "@6@nostart@update\"/>\n");
 			out.write("<listEntry value=\"mvn:org.universAAL.middleware/mw.bus.model@3\"/>\n");
 			out.write("<listEntry value=\"mvn:org.universAAL.middleware/mw.data.representation@4\"/>\n");
-		//	out.write("<listEntry value=\"mvn:org.universAAL.ontology/ont.weather@5@update\"/>\n");
+			
 			out.write("<listEntry value=\"wrap:mvn:org.osgi/osgi_R4_compendium/1.0@2\"/>\n");
 			out.write("<listEntry value=\"wrap:mvn:org.ops4j.pax.logging/pax-logging-api/1.4@2\"/>\n");
 			out.write("</listAttribute>\n");
@@ -176,8 +179,6 @@ public class CreateConfigurationFile {
 			out.write("<booleanAttribute key=\"tracing\" value=\"false\"/>\n");
 			out.write("<booleanAttribute key=\"useDefaultConfigArea\" value=\"false\"/>\n");
 			out.write("</launchConfiguration>\n");
-
-
 
 			// Close the output stream
 			out.close();
