18dd7beefe922026e7bb4cbc53b4edd9e5e49e67
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2011-05-26 15:05:39 +0000

    added new tool: log monitor

---
+++ log_monitor/org.universAAL.tools.logmonitor/src/main/java/org/universAAL/tools/logmonitor/LogMonitor.java
@@ -0,0 +1,45 @@
+/*
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+*/
+package org.universAAL.tools.logmonitor;
+
+import java.util.LinkedList;
+
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.util.LogListener;
+
+/**
+ * Implementation of the {@link org.universAAL.middleware.util.LogListener}
+ * interface to be called from {@link org.universAAL.middleware.util.LogUtils}
+ * in the bundle mw.data.representation.
+ * 
+ * @author cstockloew
+ *
+ */
+public class LogMonitor implements LogListener {
+	
+	/**
+	 * The main frame.
+	 */
+	private RDFVis vis = new RDFVis();
+
+	/**
+	 * @see org.universAAL.middleware.util.LogListener
+	 */
+	public void logDebug(String cls, String method, Object[] msgPart,
+			Throwable t) {
+		
+		// TODO: put this in a separate thread?
+		String msg = "";
+		LinkedList<Resource> lst = new LinkedList<Resource>();
+		for (int i=0; i<msgPart.length; i++) {
+			Object o = msgPart[i];
+			msg += o;
+			if (o instanceof Resource)
+				lst.add((Resource)o);
+		}
+		for (Resource r : lst)
+			vis.addMessage(cls, method, msg, r);
+	}
+}
---
+++ log_monitor/org.universAAL.tools.logmonitor/src/main/java/org/universAAL/tools/logmonitor/RDFVis.java
@@ -0,0 +1,334 @@
+/*
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+*/
+
+package org.universAAL.tools.logmonitor;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
+import org.universAAL.middleware.rdf.Resource;
+
+/**
+ * The main frame containing the list of all messages/log entry and the panel
+ * for graphical drawing of the graph.
+ * 
+ * @author cstockloew
+ *
+ */
+public class RDFVis extends JFrame implements ListSelectionListener {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * The instance of this class for calling from static methods.
+	 */
+	private static RDFVis instance;
+	
+	//private String dir = "c:\\";
+	
+	/**
+	 * All information of one log entry.
+	 */
+	private class Message {
+		/**
+		 * The date.
+		 */
+		Date date;
+		
+		/**
+		 * The class which has called the logger.
+		 */
+		String cls;
+		
+		/**
+		 * The method in which the logger was called.
+		 */
+		String method;
+		
+		/**
+		 * The actual message.
+		 */
+		String message;
+		
+		/**
+		 * The root node; corresponds to the Resource given to the logger.
+		 */
+		Node node;
+		
+		/**
+		 * Get the date as a string.
+		 * @return The date as a string.
+		 */
+		String getDateString() {
+			Calendar c = new GregorianCalendar();
+			c.setTime(date);
+			String dateString = new String();
+			dateString += c.get(Calendar.YEAR)+"-";
+			dateString += c.get(Calendar.MONTH)+"-";
+			dateString += c.get(Calendar.DAY_OF_MONTH)+" ";
+			dateString += c.get(Calendar.HOUR_OF_DAY)+":";
+			dateString += c.get(Calendar.MINUTE)+":";
+			dateString += c.get(Calendar.SECOND)+".";
+			dateString += c.get(Calendar.MILLISECOND);
+			return dateString;
+		}
+	}
+
+	/**
+	 * The graphics panel where the node graph is drawn.
+	 */
+	GraphPanel panel = new GraphPanel();
+	
+	/**
+	 * The table containing a list of all messages.
+	 */
+	JTable table;
+	
+	/**
+	 * The list of all messages.
+	 */
+	LinkedList<Message> messages = new LinkedList<Message>();
+	
+	
+	/**
+	 * Create the main frame.
+	 */
+	public RDFVis() {
+		super("RDF Visualizer");
+		
+		// RDF View
+		JScrollPane scrollpane = new JScrollPane(panel);
+		//getContentPane().add(scrollpane, BorderLayout.CENTER);
+		scrollpane.setVerticalScrollBarPolicy(
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollpane.setHorizontalScrollBarPolicy(
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		// Message overview
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Date");
+		model.addColumn("Class");
+		model.addColumn("Method");
+		model.addColumn("Message");
+		table = new JTable(model);
+		//getContentPane().add(table, BorderLayout.WEST);
+		JScrollPane scrollpaneTable = new JScrollPane(table);
+//		table.getColumnModel().getColumn(0).setPreferredWidth(120);
+//		table.getColumnModel().getColumn(1).setPreferredWidth(20);
+//		table.getColumnModel().getColumn(2).setPreferredWidth(100);
+		table.getSelectionModel().addListSelectionListener(this);
+		
+		// Split pane
+		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				scrollpaneTable, scrollpane);
+		getContentPane().add(split, BorderLayout.CENTER);
+		split.setDividerLocation(300);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1000, 700);
+		setVisible(true);
+		
+		instance = this;
+	}
+	
+	/**
+	 * Save all graphs as images.
+	 */
+	static void saveAll() {
+		if (instance != null) {
+
+			//File testFile = new File("WriteData2.dat");
+			//System.out.println("testFile=" + testFile.getAbsolutePath());
+
+			// don't use JDirectoryChooser, it hangs!!!
+			//File dir = JDirectoryChooser.showDialog(singleton);
+//			JFileChooser chooser = new JFileChooser();//instance.dir);
+//			chooser.setDialogTitle("Select target directory");
+//			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+//			if (chooser.showOpenDialog(instance)!=JFileChooser.APPROVE_OPTION)
+//				return;
+//			File myDir = chooser.getSelectedFile();
+//			if (myDir == null)
+//				return;
+//			instance.dir = ""+myDir;
+			String myDir = "";
+			
+			synchronized (instance.messages) {
+				String lastFilename = "";
+				int i=0;
+				Node node = instance.panel.node;
+				for (Message m : instance.messages) {
+					instance.panel.show(m.node);
+
+					// calc width and height, just draw everything
+					// (performance?)
+					BufferedImage bi;
+					bi = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+					Graphics2D g2 = bi.createGraphics();
+					Point p = instance.panel.drawNodes(g2, m.node, 20, 20,
+							new Point(0,0));
+					p.y += 20;
+					
+					// draw
+					bi = new BufferedImage(p.x, p.y,
+							BufferedImage.TYPE_INT_RGB);
+					g2 = bi.createGraphics();
+					g2.setColor(Color.WHITE);
+					g2.fillRect(0, 0, p.x, p.y);
+					instance.panel.drawNodes(g2, m.node, 20, 20,
+							new Point(0,0));
+					
+					// save
+					String filename = myDir //+ "\\"
+							+ m.getDateString().replace(':', '-') + "_"
+							+ m.cls + "_"
+							+ m.method + "_"
+							+ m.message.replaceAll("[\\\\/:*?\"<>|]", "_");
+					if (filename.equals(lastFilename)) {
+						lastFilename = filename;
+						filename += "_" + i;
+						i++;
+					} else {
+						lastFilename = filename;
+						i=0;
+					}
+					filename += ".png";
+					
+					File outFile = new File(filename);
+					System.out.println("File: width " + p.x + " height " + p.y
+							+ " "
+							+ outFile.getAbsolutePath() + outFile.getName());
+					try {
+						ImageIO.write(bi, "png", outFile);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				instance.panel.show(node);
+			}
+		}
+	}
+
+	/**
+	 * Add a new log entry.
+	 * 
+	 * @param cls
+	 *            The class which has called the logger.
+	 * @param method
+	 *            The method in which the logger was called.
+	 * @param message
+	 *            The actual message.
+	 * @param r
+	 *            The {@link org.universAAL.middleware.rdf.Resource}
+	 */
+	public void addMessage(String cls, String method, String message,
+			Resource r) {
+
+		Node node = Node.copyFrom(r);
+		addMessage(cls, method, message, node, new Date());
+	}
+		
+	/**
+	 * Add a new log entry.
+	 * 
+	 * @param cls
+	 *            The class which has called the logger.
+	 * @param method
+	 *            The method in which the logger was called.
+	 * @param message
+	 *            The actual message.
+	 * @param node
+	 *            The root node.
+	 * @param date
+	 *            The date.
+	 */
+	private void addMessage(String cls, String method, String message,
+			Node node, Date date) {
+		
+		synchronized (messages) {
+			Message m = new Message();
+			m.message = message;
+			m.method = method;
+			m.date = date;
+			m.node = node;
+			m.cls = cls;
+			messages.add(m);
+			
+			DefaultTableModel model = (DefaultTableModel) table.getModel();
+			model.addRow(
+					new Object[]{m.getDateString(), cls, method, message});
+			
+			//panel.show(node);
+		}
+	}
+	
+	/**
+	 * Called whenever the value of the selection in the table showing the list
+	 * of all messages changes.
+	 * 
+	 * @param e
+	 *            The event that characterizes the change.
+	 */
+	public void valueChanged(ListSelectionEvent e) {
+		if (!e.getValueIsAdjusting()) {
+			int viewRow = table.getSelectedRow();
+			if (viewRow >= 0) {
+				Message m = messages.get(viewRow);
+				panel.show(m.node);
+			}
+		}
+	}
+
+	// main method for testing
+	public static void main(String[] args) {
+		RDFVis frame = new RDFVis();
+		
+		Node n = new Node("root");
+		for (int i=0; i<4; i++)
+			n.addChild("link2child"+i, new Node("child"+i));
+		n.children.get(0).getChild().theClass = String.class;
+		n.children.get(1).getChild().addChild("link2child_1_1", new Node("child_1_1"));
+		n.children.get(2).getChild().addChild("link2child_2_1ccccc", n, true);
+		n.children.get(3).getChild().addChild("link2child_3_1", n, true);
+		frame.addMessage("","","4", n, new Date());
+		
+		n = new Node("root");
+		frame.addMessage("","","single", n, new Date());
+		
+		n = new Node("root");
+		n.addChild("a1", new Node("a1"));
+		n.addChild("a5", new Node("a5"));
+		n.addChild("a3", new Node("a3"));
+		n.addChild(Edge.typeString, new Node(Edge.typeString));
+		n.addChild(Edge.typeString, new Node(Edge.typeString));
+		n.addChild("a3", new Node("a3"));
+		n.addChild("a3", new Node("a3"));
+		n.addChild("z2", new Node("z2"));
+		n.addChild("z9", new Node("z9"));
+		n.addChild("b", new Node("b"));
+		Collections.sort(n.children);
+		frame.addMessage("","","sort", n, new Date());
+	}
+}
