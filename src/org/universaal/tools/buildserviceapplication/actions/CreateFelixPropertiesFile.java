f0ab7ed964c1716cc458677b8353514852b47120
Konstantinos Giannoutakis <kgiannou@iti.gr>
2011-06-24 14:20:53 +0000

    redundant imports removed

--- src/org/universaal/tools/buildserviceapplication/actions/CreateFelixPropertiesFile.java
+++ src/org/universaal/tools/buildserviceapplication/actions/CreateFelixPropertiesFile.java
@@ -2,34 +2,16 @@ package org.universaal.tools.buildserviceapplication.actions;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.maven.cli.MavenCli;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jdt.core.IClasspathContainer;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
+
 
 public class CreateFelixPropertiesFile {
 	static public String groupId = "";
