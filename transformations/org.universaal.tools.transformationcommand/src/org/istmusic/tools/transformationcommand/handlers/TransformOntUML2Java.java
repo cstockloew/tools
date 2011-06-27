f0b342eb3c1f22f007337a4c9fbfe7527e05fa7e
Erlend Stav <erlend.stav@sintef.no>
2011-06-27 09:55:46 +0000

    Added new command for transformation from UML ontology to Java

---
+++ transformations/org.universaal.tools.transformationcommand/src/org/istmusic/tools/transformationcommand/handlers/TransformOntUML2Java.java
@@ -0,0 +1,10 @@
+package org.istmusic.tools.transformationcommand.handlers;
+
+public class TransformOntUML2Java extends TransformationHandler {
+	static final String TRANSFORMATION_FILENAME = "ontUML2Java.m2t";
+	static final String THIS_BUNDLE_NAME = "org.universaal.tools.transformationcommand";
+
+	public TransformOntUML2Java() {
+		setFileAndBundleName(TRANSFORMATION_FILENAME, THIS_BUNDLE_NAME);
+	}
+}
