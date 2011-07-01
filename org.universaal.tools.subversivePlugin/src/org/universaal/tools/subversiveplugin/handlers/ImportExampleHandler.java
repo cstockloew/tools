f47e805b823d8020711e663916b16704335b3dfe
Erlend Stav <erlend.stav@sintef.no>
2011-07-01 17:09:35 +0000

    This versions in this tag are the tools present in the update site for the Prototype v0.5. July 01, 2011

---
+++ org.universaal.tools.subversivePlugin/src/org/universaal/tools/subversiveplugin/handlers/ImportExampleHandler.java
@@ -0,0 +1,26 @@
+package org.universaal.tools.subversiveplugin.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.universaal.tools.subversiveplugin.wizards.ImportExampleWizard;
+
+public class ImportExampleHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(arg0);
+		ImportExampleWizard wiz = new ImportExampleWizard(window);
+
+		WizardDialog dialog = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+				wiz);
+		dialog.create();
+		dialog.open();
+		return null;
+	}
+
+}
