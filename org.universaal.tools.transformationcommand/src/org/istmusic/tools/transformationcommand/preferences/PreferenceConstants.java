de68bdbbb34258cdc85acef1af0bc4d8aad0dc7d
Erlend Stav <erlend.stav@sintef.no>
2011-07-01 17:09:18 +0000

    This versions in this tag are the tools present in the update site for the Prototype v0.5. July 01, 2011

---
+++ org.universaal.tools.transformationcommand/src/org/istmusic/tools/transformationcommand/preferences/PreferenceConstants.java
@@ -0,0 +1,8 @@
+package org.istmusic.tools.transformationcommand.preferences;
+
+public class PreferenceConstants {
+	
+	public static final String P_UML2JAVA_PATH = "uml2javaabsolutepath";
+	public static final String P_UML2JAVA_ABSOLUTE_BOOLEAN = "uml2javarelativepathboolean";
+
+}
