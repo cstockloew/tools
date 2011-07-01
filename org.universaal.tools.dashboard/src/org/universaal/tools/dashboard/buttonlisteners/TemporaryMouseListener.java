4e265dc3e1810c40c6d1902586a1eb785f7eef26
Erlend Stav <erlend.stav@sintef.no>
2011-07-01 17:08:35 +0000

    This versions in this tag are the tools present in the update site for the Prototype v0.5. July 01, 2011

---
+++ org.universaal.tools.dashboard/src/org/universaal/tools/dashboard/buttonlisteners/TemporaryMouseListener.java
@@ -0,0 +1,39 @@
+package org.universaal.tools.dashboard.buttonlisteners;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.ui.part.ViewPart;
+
+public class TemporaryMouseListener implements MouseListener {
+
+	ViewPart view;
+	String message;
+	
+	public TemporaryMouseListener(ViewPart view, String input){
+		this.view = view;
+		this.message = input;
+	}
+	
+	@Override
+	public void mouseDoubleClick(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDown(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseUp(MouseEvent arg0) {
+		MessageDialog.openInformation(
+				view.getSite().getShell(),
+				"Sample View",
+				message);
+
+	}
+
+}
---
+++ org.universaal.tools.dashboard/src/org/universaal/tools/dashboard/listeners/ProjectNameListener.java
@@ -0,0 +1,67 @@
+package org.universaal.tools.dashboard.listeners;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.internal.core.PackageFragment;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.universaal.tools.dashboard.views.DashboardView;
+
+public class ProjectNameListener implements ISelectionListener {
+	
+	IWorkbenchPart part;
+	IWorkbenchPart source;
+	ISelection selection;
+	
+	public ProjectNameListener(IWorkbenchPart source){
+		this.source = source;
+	}
+
+	@Override
+	public void selectionChanged(IWorkbenchPart arg0, ISelection arg1) {
+		this.part = arg0;
+		this.selection = arg1;
+		pageSelectionChanged();
+
+	}
+	
+	protected void pageSelectionChanged(){
+		if(part == source){
+			return;
+		}
+		if(!(selection instanceof IStructuredSelection)){
+			return;
+		}
+		IStructuredSelection sel = (IStructuredSelection)selection;
+		IProject project=null;
+		Object element;
+		IPath path;
+		
+		element = sel.getFirstElement();
+		
+		if(element instanceof IResource){
+			project = ((IResource)element).getProject();
+		}else if (element instanceof PackageFragment){
+			IJavaProject jProject = ((PackageFragment)element).getJavaProject();
+			project = jProject.getProject();
+		}else if (element instanceof IJavaElement){
+			IJavaProject jProject = ((IJavaElement)element).getJavaProject();
+			project = jProject.getProject();
+		}
+		
+		if(project!=null){
+			path = project.getFullPath();
+			((DashboardView)source).setProjectName(path.toPortableString());
+		}
+		
+		
+		
+	}
+
+}
