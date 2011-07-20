08159f381f14bcf4c8e6ff81420927b18c57733d
Mr Adrian Norås <adrian.corma@gmail.com>
2011-07-20 08:28:37 +0000

    Added license information. Renamed packages to fit naming convention.

--- depotclient/org.universaal.tools.xmlEditor/src/org/universaal/tools/xmleditor/editors/XmlEditor.java
+++ depotclient/org.universaal.tools.xmlEditor/src/org/universaal/tools/xmleditor/editors/XmlEditor.java
@@ -1,4 +1,22 @@
-package xmleditor.editors;
+/*
+	Copyright 2011 SINTEF, http://www.sintef.no
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universaal.tools.xmleditor.editors;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -45,6 +63,7 @@ import org.eclipse.ui.part.MultiPageEditorPart;
 import org.eclipse.ui.part.MultiPageEditorSite;
 import org.eclipse.wst.sse.ui.StructuredTextEditor;
 import org.eclipse.wst.xml.core.internal.provisional.contenttype.ContentTypeIdForXML;
+import org.universaal.tools.xmleditor.model.ProjectModel;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -53,7 +72,6 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import xmleditor.model.ProjectModel;
 
 public class XmlEditor extends MultiPageEditorPart implements IResourceChangeListener{
 
