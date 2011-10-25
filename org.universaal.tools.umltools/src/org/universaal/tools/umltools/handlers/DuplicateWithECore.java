f0e7d4feba013dfe177323226edfa26247998fe0
Erlend Stav <erlend.stav@sintef.no>
2011-10-25 13:56:03 +0000

    Added draft code that duplicates also the .di and .notation files of Papyrus

--- org.universaal.tools.umltools/src/org/universaal/tools/umltools/handlers/DuplicateWithECore.java
+++ org.universaal.tools.umltools/src/org/universaal/tools/umltools/handlers/DuplicateWithECore.java
@@ -2,25 +2,41 @@ package org.universaal.tools.umltools.handlers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.uml2.uml.Model;
 import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.resource.UMLResource;
+
+import org.eclipse.papyrus.resource.ModelException;
+import org.eclipse.papyrus.resource.ModelSet;
+import org.eclipse.papyrus.resource.ModelsReader;
+
+
+import org.eclipse.papyrus.core.resourceloading.OnDemandLoadingModelSet;
+
+
+
 
 
 public class DuplicateWithECore extends AbstractHandler {
@@ -28,12 +44,36 @@ public class DuplicateWithECore extends AbstractHandler {
 	Shell shell;
 	
 
-	protected static ResourceSet createAndInitResourceSet() {
+	protected static ModelSet createAndInitResourceSet() {
 		// Create a resource set and register XMI resource factory
 		// associated with ".simple" file extension
-		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ubicompdescriptor", new XMIResourceFactoryImpl());
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("simplelanguage", new XMIResourceFactoryImpl());
+		ModelSet resourceSet = new ModelSet();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put
+        (Resource.Factory.Registry.DEFAULT_EXTENSION, 
+   	         new XMIResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+				UMLResource.FILE_EXTENSION, UMLResource.Factory.INSTANCE);
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+				"ecore", new EcoreResourceFactoryImpl());
+		
+		//resourceSet.
+		/*		ResourceSet resourceSet = new ResourceSetImpl();
+		
+//		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("simplelanguage", new XMIResourceFactoryImpl());
+
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put
+	        (Resource.Factory.Registry.DEFAULT_EXTENSION, 
+	         new XMIResourceFactoryImpl());		
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+				UMLResource.FILE_EXTENSION, UMLResource.Factory.INSTANCE);
+
+
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+				//GMFReource.
+				UMLResource.FILE_EXTENSION, UMLResource.Factory.INSTANCE);
+
+*/	
+		
 		return resourceSet;
 	}
 
@@ -42,13 +82,101 @@ public class DuplicateWithECore extends AbstractHandler {
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		
+		
+/*		org.eclipse.papyrus.sashwindows.di.DiPackage.eINSTANCE;
+		impl.DiPackageImpl.init();
+		org.eclipse.gmf.runtime.notation.
+*/		
+		
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		this.shell = window.getShell();
 		MessageDialog.openInformation(
 				shell,
-				"UML tools",
+				"Creating from template files",
 				"UML model created!");
 
+		
+		//OnDemandLoadingModelSet theSet = new OnDemandLoadingModelSet();
+		
+		
+		//org.eclipse.papyrus.core.utils.PapyrusEcoreUtils utils;
+		
+		//org.eclipse.papyrus.core.resourceloading.
+		
+
+		org.eclipse.papyrus.resource.ModelsReader reader;
+		org.eclipse.papyrus.sashwindows.di.impl.DiPackageImpl pack;
+		
+		
+		FileDialog fd = new FileDialog(shell); 
+		
+
+		fd.setFilterExtensions(new String[] {"*.di"});
+		fd.setFilterNames(new String[] {"Papyrus model"});
+		
+//		fd.setFilterExtensions(new String[] {"*.notation"});
+//		fd.setFilterNames(new String[] {"Papyrus notation file"});
+		
+		String ret = fd.open();
+		
+		if(ret!=null){
+			ModelSet resourceSet = createAndInitResourceSet(); 
+			
+		
+			
+			EObject diRoot = loadRootObject(resourceSet, ret);
+			EObject notationRoot = loadRootObject(resourceSet, ret.replace(".di", ".notation"));
+			EObject umlRoot = loadRootObject(resourceSet, ret.replace(".di", ".uml"));
+
+			List<Resource> allRes = resourceSet.getResources();
+			List<EObject> newList = new ArrayList<EObject>();
+			for (int i = 0; i < allRes.size(); i++) {
+				newList.add(allRes.get(i).getContents().get(0)); // Is any more explicit loading needed?
+			}
+			Collection<EObject> copyList = EcoreUtil.copyAll(newList);
+
+
+			// Ask for name of new model
+			FileDialog fdSave = new FileDialog(shell,SWT.SAVE); 
+
+			fdSave.setFilterExtensions(new String[] {"*.di"});
+			fdSave.setFilterNames(new String[] {"Papyrus model"});
+
+			String notationFileName = fdSave.open();
+
+			if(notationFileName!=null){
+
+
+				// Create a new resource set for the copies, and add the each root object to a new resource in this set 
+				ResourceSet newResourceSet = createAndInitResourceSet();
+				for (Iterator<EObject> iter = copyList.iterator(); iter.hasNext(); ) {
+					EObject cr = iter.next();
+					String saveFileName = notationFileName;
+					if (cr instanceof Model) {
+						saveFileName = saveFileName.replace(".di", ".uml");
+					} else if (cr.getClass().getName().contains("Diagram")) {
+						saveFileName = saveFileName.replace(".di", ".notation");						
+					}
+					URI uri = URI.createFileURI(saveFileName);
+					Resource resource = newResourceSet.createResource(uri);
+					resource.getContents().add(cr);
+				}
+
+				// Save the resources
+				EList<Resource> resources = newResourceSet.getResources();
+				for (Iterator<Resource> resIter = resources.iterator(); resIter.hasNext(); ) {					
+					try {
+						resIter.next().save(null);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}	
+		}
+		
+		
 /*		Model myModel = createModel("My model");				
 		importPrimitiveTypes(myModel);
 		
@@ -139,16 +267,15 @@ public class DuplicateWithECore extends AbstractHandler {
 	 * @param fileURI
 	 * @return The loaded root object, or null if no root object could not be found
 	 */
-	public static Object loadRootObject(String fileURI) {
+	public static EObject loadRootObject(ResourceSet resourceSet, String fileURI) {
 		
 		if(fileURI !=null){
-			ResourceSet resourceSet = createAndInitResourceSet(); 
-			
 			//Set the file name from the dialog
 			URI uri = URI.createFileURI(fileURI);
 			Resource resource = resourceSet.getResource(uri, true);
 			try {
-				Object userService = resource.getContents().get(0);
+				resource.load(null);
+				EObject userService = resource.getContents().get(0);
 
 				return userService;				
 			}
