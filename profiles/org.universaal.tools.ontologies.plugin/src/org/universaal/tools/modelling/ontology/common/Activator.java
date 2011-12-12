dbca19f0915c5a4b4bbce5b8bec84638fe90dd90
Erlend Stav <erlend.stav@sintef.no>
2011-12-12 13:25:43 +0000

    Updated package name to new naming convention for modelling related projects

--- profiles/org.universaal.tools.ontologies.plugin/src/org/universaal/tools/modelling/ontology/common/Activator.java
+++ profiles/org.universaal.tools.ontologies.plugin/src/org/universaal/tools/modelling/ontology/common/Activator.java
@@ -1,4 +1,4 @@
-package org.universaal.tools.ontologies.plugin;
+package org.universaal.tools.modelling.ontology.common;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.universaal.tools.ontologies.plugin"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.universaal.tools.modelling.ontology.common"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
