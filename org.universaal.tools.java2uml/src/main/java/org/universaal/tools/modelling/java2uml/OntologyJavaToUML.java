ff548cf9196c997a2c8df1d19f86dbc5a1f47509
Erlend Stav <erlend.stav@sintef.no>
2012-06-29 09:09:33 +0000

    Now uses UIBus ontology as example

--- org.universaal.tools.java2uml/src/main/java/org/universaal/tools/modelling/java2uml/OntologyJavaToUML.java
+++ org.universaal.tools.java2uml/src/main/java/org/universaal/tools/modelling/java2uml/OntologyJavaToUML.java
@@ -5,12 +5,13 @@ import org.universAAL.middleware.owl.ObjectProperty;
 import org.universAAL.middleware.owl.OntClassInfo;
 import org.universAAL.middleware.owl.OntologyManagement;
 import org.universAAL.middleware.rdf.Property;
+import org.universAAL.middleware.ui.owl.UIBusOntology;
 import org.universAAL.ontology.ProfileOntology;
 
 public class OntologyJavaToUML {
 
-	private static ProfileOntology ontology = new ProfileOntology();
-	//private static UIBusOntology ontology = new UIBusOntology();
+	//private static ProfileOntology ontology = new ProfileOntology();
+	private static UIBusOntology ontology = new UIBusOntology();
 
 	public static void main(String[] args) {
 		OntologyManagement.getInstance().register(ontology);
