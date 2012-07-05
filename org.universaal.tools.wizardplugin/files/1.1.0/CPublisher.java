8b847009047d54e5637ef7f08d501c5439751fad
Erlend Stav <erlend.stav@sintef.no>
2012-07-05 08:20:14 +0000

    Release for D3.1-C

---
+++ org.universaal.tools.wizardplugin/files/1.1.0/CPublisher.java
@@ -0,0 +1,31 @@
+/*TAG:PACKAGE*/
+
+/* More on how to use this class at: 
+ * http://forge.universaal.org/wiki/support:Developer_Handbook_6#Publishing_context_events */
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.context.ContextPublisher;
+import org.universAAL.middleware.context.owl.ContextProvider;
+
+public class /*TAG:CLASSNAME*/ extends ContextPublisher {
+
+    protected /*TAG:CLASSNAME*/(ModuleContext context, ContextProvider providerInfo) {
+	super(context, providerInfo);
+	// TODO Auto-generated constructor stub
+    }
+
+    protected /*TAG:CLASSNAME*/(ModuleContext context) {
+	super(context, getProviderInfo());
+	// TODO Auto-generated constructor stub
+    }
+
+    private static ContextProvider getProviderInfo() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    public void communicationChannelBroken() {
+	// TODO Auto-generated method stub
+
+    }
+
+}
---
+++ org.universaal.tools.wizardplugin/src/org/universaal/tools/newwizard/plugin/commands/NewProjectCommandHandler.java
@@ -0,0 +1,47 @@
+package org.universaal.tools.newwizard.plugin.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.wizards.IWizardDescriptor;
+import org.universaal.tools.newwizard.plugin.wizards.Messages;
+
+public class NewProjectCommandHandler extends AbstractHandler {
+
+    // Executed when command is called
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+	// Get the wizard by its name
+	IWizardDescriptor descriptor = PlatformUI
+		.getWorkbench()
+		.getNewWizardRegistry()
+		.findWizard(
+			"org.universaal.tools.newwizard.plugin.wizards.NewProjectWizard");
+
+	try {
+	    // Get the current (the main) window and start the wizard
+	    IWorkbenchWindow window = HandlerUtil
+		    .getActiveWorkbenchWindowChecked(event);
+	    if (descriptor != null) {
+		IWizard wizard = descriptor.createWizard();
+		WizardDialog wd = new WizardDialog(window.getShell(), wizard);
+		wd.setTitle(wizard.getWindowTitle());
+		wd.open();
+	    } else {
+		MessageDialog.openInformation(window.getShell(), Messages.getString("Command.0"),
+			Messages.getString("Command.1"));
+	    }
+	} catch (CoreException e) {
+	    e.printStackTrace();
+	}
+
+	return null;
+    }
+
+}
