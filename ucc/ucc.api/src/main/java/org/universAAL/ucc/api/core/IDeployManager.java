f2d4af191b0b9a6ec963399f33d7dc9ef2fe08bc
Shanshan Jiang <shanshan.jiang@sintef.no>
2012-11-29 13:18:48 +0000

    update after telco 26.11.2012

--- ucc/ucc.api/src/main/java/org/universAAL/ucc/api/core/IDeployManager.java
+++ ucc/ucc.api/src/main/java/org/universAAL/ucc/api/core/IDeployManager.java
@@ -11,60 +11,56 @@ import java.util.Map;
  */
 public interface IDeployManager {
 	/**
-	 * install a service for a user as specified in the .usrv file
-	 * @param username
-	 * @param password
-	 * @param usrvfile: the link to download the .usrv file 
+	 * install a service as specified in the .usrv file
+	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
+	 * @param usrvfile: the link to download the .usrv file, serviceId from uStore is provided in .usrv file. 
 	 */
-	public void install(String username, String password, URL usrvfile);
+	public void install(String sessionKey, URL usrvfile);
 	
 	/**
-	 * update a service for a user as specified in the .usrv file
-	 * @param username
-	 * @param password
-	 * @param usrvfile: the link to download the .usrv file
+	 * update a service as specified in the .usrv file
+	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
+	 * @param usrvfile: the link to download the .usrv file, serviceId from uStore is provided in .usrv file. 
 	 */
 	public void update(String username, String password, URL usrvfile);
 	
 	/**
 	 * uninstall a service 
-	 * @param username
-	 * @param password
+	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
 	 * @param serviceId: the uStore service id for the service to be uninstalled 
-	 * 
+	 *  
 	 */
-	public void uninstall(String username, String password, String serviceId, String serviceVersion);
+	public void uninstall(String sessionKey, String serviceId);
 	
 	/**
-	 * get all installed services for a user
-	 * @param username
-	 * @param password
-	 * @return Map<serviceId, serviceVersion>: the list of services with uStore service Id and its version
+	 * get all installed services 
+	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
+	 * @return Map<serviceId, serviceVersion>: the list of services with uStore service Id (and its version?)
 	 */
-	public 	Map<String, String> getInstalledServices(String username, String password);
+	public 	Map<String, String> getInstalledServices(String sessionKey);
 
 	/**
-	 * get the installed applications for a service 
-	 * @param username
-	 * @param password
+	 * get the installed application units for a service 
+	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
 	 * @param serviceId
-	 * @return Map<applicationId, appVersion>: the list of applications with their version
+	 * @return Map<bundleId, bundleVersion>: the list of application bundles with their version
+	 * 
 	 */
-	public Map<String, String> getInstalledApplications(String username, String password, String serviceId);
+	public Map<String, String> getInstalledUnitsForService(String sessionKey, String serviceId);
 
 	/**
 	 * get the AAL space profile 
 	 * @return
 	 * TODO: decide what to return, whether to use AALSpaceCard or AALSpaceDescriptor as defined in 
-	 * the mw.interfaces.aalspace
+	 * the mw.interfaces.aalspace.
+	 * Suggest to return a list of properties of capabilities and functionalities in the form of <propertyName, propValue, criteria>
 	 */
 	public List getAALSpaceProfile();
 	
 	/**
 	 * 
-	 * @param username
-	 * @param password
+	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
 	 * TODO: decide what to return, what info should be contained in a user profile 
 	 */
-	public List getUserProfile(String username, String password);
+	public List getUserProfile(String sessionKey);
 }
