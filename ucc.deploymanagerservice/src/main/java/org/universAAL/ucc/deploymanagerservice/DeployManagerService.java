9337d554dbda4b5276dc5546bcae6645ffbe5ab0
Shanshan Jiang <shanshan.jiang@sintef.no>
2013-01-17 10:33:03 +0000

    update WS API to use only String for parameters and return type.

--- ucc.deploymanagerservice/src/main/java/org/universAAL/ucc/deploymanagerservice/DeployManagerService.java
+++ ucc.deploymanagerservice/src/main/java/org/universAAL/ucc/deploymanagerservice/DeployManagerService.java
@@ -1,9 +1,5 @@
 package org.universAAL.ucc.deploymanagerservice;
 
-import java.net.URL;
-import java.util.List;
-import java.util.Map;
-
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
@@ -19,15 +15,13 @@ public interface DeployManagerService {
 	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
 	 * @param usrvfile: the link to download the .usrv file, serviceId from uStore is provided in .usrv file. 
 	 */
-	public void install(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "usrvfile") URL usrvfile);
-	
+	public void install(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "usrvfile") String usrvfile);
 	/**
 	 * update a service as specified in the .usrv file
 	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
 	 * @param usrvfile: the link to download the .usrv file, serviceId from uStore is provided in .usrv file. 
 	 */
-	public void update(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "usrvfile") URL usrvfile);
-	
+	public void update(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "usrvfile") String usrvfile);
 	/**
 	 * uninstall a service 
 	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
@@ -35,36 +29,44 @@ public interface DeployManagerService {
 	 *  
 	 */
 	public void uninstall(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "serviceId") String serviceId);
-	
 	/**
 	 * get all installed services 
 	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
-	 * @return List<serviceId>: list of uStore service Ids
+	 * @return The list of uStore service Ids in the following template:
+	 * <services>
+	 * 		<service>serviceId</service>
+	 * 		<service>serviceId</service>
+	 * 		...
+	 * </services>
 	 */
-	public 	List<String> getInstalledServices(@WebParam(name = "sessionKey") String sessionKey);
+	public String getInstalledServices(@WebParam(name = "sessionKey") String sessionKey);
 
 	/**
 	 * get the installed application units for a service 
 	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
 	 * @param serviceId
-	 * @return Map<bundleId, bundleVersion>: the list of application bundles with their version
-	 * 
+	 * @return The list of application bundles with their version in the following template:
+	 * <serviceUnits>
+	 * 		<unit><id>bundleId</id><version>bundleVersion</version></unit>
+	 * 		<unit><id>bundleId</id><version>bundleVersion</version></unit>
+	 * 		...
+	 * </serviceUnits>
 	 */
-	public Map<String, String> getInstalledUnitsForService(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "serviceId") String serviceId);
-
+	public String getInstalledUnitsForService(@WebParam(name = "sessionKey") String sessionKey, @WebParam(name = "serviceId") String serviceId);
 	/**
 	 * get the AAL space profile 
-	 * @return
+	 * @return The AAL space profile in the following template:
 	 * TODO: decide what to return, whether to use AALSpaceCard or AALSpaceDescriptor as defined in 
 	 * the mw.interfaces.aalspace.
 	 * Suggest to return a list of properties of capabilities and functionalities in the form of <propertyName, propValue, criteria>
 	 */
-	public List getAALSpaceProfile();
+	public String getAALSpaceProfile();
 	
 	/**
 	 * 
 	 * @param sessionKey: the sessionKey for the interaction obtained when uCC registers with uStore 
+	 * @return The user profile in the following template:
 	 * TODO: decide what to return, what info should be contained in a user profile 
 	 */
-	public List getUserProfile(@WebParam(name = "sessionKey") String sessionKey);
+	public String getUserProfile(@WebParam(name = "sessionKey") String sessionKey);
 }
