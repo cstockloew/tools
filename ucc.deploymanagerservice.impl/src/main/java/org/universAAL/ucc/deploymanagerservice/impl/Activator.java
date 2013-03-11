0b123292f16ef9b88d60367f57571bb2363810b3
Shanshan Jiang <shanshan.jiang@sintef.no>
2013-03-11 15:57:18 +0000

    connect to Deinstaller of ucc.controller. update POM version

--- ucc.deploymanagerservice.impl/src/main/java/org/universAAL/ucc/deploymanagerservice/impl/Activator.java
+++ ucc.deploymanagerservice.impl/src/main/java/org/universAAL/ucc/deploymanagerservice/impl/Activator.java
@@ -8,12 +8,14 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
+import org.universAAL.ucc.api.IDeinstaller;
 import org.universAAL.ucc.api.IInstaller;
 import org.universAAL.ucc.deploymanagerservice.DeployManagerService;
 
 public class Activator implements BundleActivator {
     private ServiceRegistration registration;
     private static IInstaller installer = null;
+    private static IDeinstaller deinstaller = null;
     private static BundleContext context;
 
     public void start(BundleContext bc) throws Exception {
@@ -41,10 +43,21 @@ public class Activator implements BundleActivator {
 					.getName());
 			if (sr != null)
 				installer = (IInstaller) bc.getService(sr);		
-			else System.out.println("[DeployManagerServiceImpl.activator] service reference is null!");
+			else System.out.println("[DeployManagerServiceImpl.activator] service reference for IInstaller is null!");
 			if (installer==null) System.out.println("[DeployManagerServiceImpl.activator] can not get installer! ");
 	    	else System.out.println("[DeployManagerServiceImpl.activator.getServices] got installer ");
 		}
+		
+		if (deinstaller == null) {
+			ServiceReference sr = bc.getServiceReference(IDeinstaller.class
+					.getName());
+			if (sr != null)
+				deinstaller = (IDeinstaller) bc.getService(sr);		
+			else System.out.println("[DeployManagerServiceImpl.activator] service reference for IDeinstaller is null!");
+			if (deinstaller==null) System.out.println("[DeployManagerServiceImpl.activator] can not get deinstaller! ");
+	    	else System.out.println("[DeployManagerServiceImpl.activator.getServices] got deinstaller ");
+		}
+		
 	}
     
     public static IInstaller getInstaller() {
@@ -53,5 +66,13 @@ public class Activator implements BundleActivator {
     	if (installer==null) System.out.println("[DeployManagerServiceImpl.activator] can not get installer! ");
     	else System.out.println("[DeployManagerServiceImpl.activator.getInstaller] got installer ");
      	return installer;
+    }  
+    
+    public static IDeinstaller getDeinstaller() {
+    	System.out.println("[DeployManagerServiceImpl.activator.getDeinstaller]");
+    	if (deinstaller==null) getServices(context);
+    	if (deinstaller==null) System.out.println("[DeployManagerServiceImpl.activator] can not get deinstaller! ");
+    	else System.out.println("[DeployManagerServiceImpl.activator.getDeinstaller] got deinstaller ");
+     	return deinstaller;
     }  
 }
--- ucc.deploymanagerservice.impl/src/main/java/org/universAAL/ucc/deploymanagerservice/impl/DeployManagerServiceImpl.java
+++ ucc.deploymanagerservice.impl/src/main/java/org/universAAL/ucc/deploymanagerservice/impl/DeployManagerServiceImpl.java
@@ -43,19 +43,25 @@ public class DeployManagerServiceImpl implements DeployManagerService {
 
 	public void update(String sessionKey, String usrvfile) {
 		System.out.println("[DeployManagerService] Update with sessionKey: " + sessionKey + " for URL: " + usrvfile);
-		try {
+/*		try {
 			URL usrvURL = new URL(usrvfile);
 		}
 		catch(MalformedURLException e) 
 		{
 			System.out.println("[ERROR] Malformed URL Exception for " + usrvfile);
-		}
+		} */
 		// TODO what check should be done with the sessionKey?
+		// get serviceId
+		// uninstall service first
+		// just for testing
+		uninstall(sessionKey, usrvfile);
+		// install the updated service
 	}
 
 	public void uninstall(String sessionKey, String serviceId) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("[DeployManagerServiceImpl] uninstall of serviceId: " + serviceId);
+		String results = Activator.getDeinstaller().uninstallServiceFromOnlineStore(serviceId);
+		System.out.println("[DeployManagerServiceImpl] the result for uninstallation is: " + results);
 	}
 
 	public String getInstalledServices(String sessionKey) {
