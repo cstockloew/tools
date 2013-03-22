9998ece729309c946d70c82d5901d99d265be536
Erlend Stav <erlend.stav@sintef.no>
2013-03-22 18:49:37 +0000

    Tagged version corresponding to the D3.1-D external proposed release of the tools

---
+++ org.universaal.tools.wizardplugin/files/0.3.0-SNAPSHOT/SCaller.java
@@ -0,0 +1,26 @@
+/*TAG:PACKAGE*/
+
+/* More on how to use this class at: 
+ * http://forge.universaal.org/wiki/support:Developer_Handbook_7 */
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.service.ServiceCaller;
+import org.universAAL.middleware.service.ServiceResponse;
+
+public class /*TAG:CLASSNAME*/ extends ServiceCaller{
+
+	protected /*TAG:CLASSNAME*/(BundleContext context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void communicationChannelBroken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void handleResponse(String reqID, ServiceResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
