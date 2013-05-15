309070e4d999dde677bcf7ae6abfb18efe00cef5
Nicole Merkle <merkle@fzi.de>
2013-05-15 09:31:54 +0000

    not necessary

--- ucc.aalspace.server/src/main/java/org/universAAL/ucc/database/listener/interfaces/OntologyChangedListener.java
+++
@@ -1,9 +0,0 @@
-package org.universAAL.ucc.database.listener.interfaces;
-
-import org.universAAL.ucc.model.jaxb.OntologyInstance;
-
-public interface OntologyChangedListener {
-	public void ontologyDeleted(OntologyInstance ont);
-	public void ontologyChanged(OntologyInstance ont);
-	public void ontologySaved(OntologyInstance ont);
-}
