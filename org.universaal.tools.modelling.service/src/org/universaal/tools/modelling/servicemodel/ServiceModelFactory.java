74831f13b90ef847ca23663ac7491439b217923f
Erlend Stav <erlend.stav@sintef.no>
2013-05-29 14:51:58 +0000

    Tagged version for the D3.1-D external revised version, including version 1.0.0 of some tools, and a 0.9.6 release candidate for the others.

---
+++ org.universaal.tools.modelling.service/src/org/universaal/tools/modelling/servicemodel/ServiceModelFactory.java
@@ -0,0 +1,103 @@
+/**
+ * 	Copyright 2013 SINTEF, http://www.sintef.no
+ * 	
+ * 	See the NOTICE file distributed with this work for additional 
+ * 	information regarding copyright ownership
+ * 	
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ * 	
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ * 	
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ */
+package org.universaal.tools.modelling.servicemodel;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see org.universaal.tools.modelling.servicemodel.ServiceModelPackage
+ * @generated
+ */
+public interface ServiceModelFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	ServiceModelFactory eINSTANCE = org.universaal.tools.modelling.servicemodel.impl.ServiceModelFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Service Interface</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Service Interface</em>'.
+	 * @generated
+	 */
+	ServiceInterface createServiceInterface();
+
+	/**
+	 * Returns a new object of class '<em>Service Operation</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Service Operation</em>'.
+	 * @generated
+	 */
+	ServiceOperation createServiceOperation();
+
+	/**
+	 * Returns a new object of class '<em>Parameter</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Parameter</em>'.
+	 * @generated
+	 */
+	Parameter createParameter();
+
+	/**
+	 * Returns a new object of class '<em>Service Effect</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Service Effect</em>'.
+	 * @generated
+	 */
+	ServiceEffect createServiceEffect();
+
+	/**
+	 * Returns a new object of class '<em>Named Element</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Named Element</em>'.
+	 * @generated
+	 */
+	NamedElement createNamedElement();
+
+	/**
+	 * Returns a new object of class '<em>Property Path</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Property Path</em>'.
+	 * @generated
+	 */
+	PropertyPath createPropertyPath();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	ServiceModelPackage getServiceModelPackage();
+
+} //ServiceModelFactory
---
+++ org.universaal.tools.modelling.service/src/org/universaal/tools/modelling/servicemodel/impl/ServiceModelFactoryImpl.java
@@ -0,0 +1,218 @@
+/**
+ * 	Copyright 2013 SINTEF, http://www.sintef.no
+ * 	
+ * 	See the NOTICE file distributed with this work for additional 
+ * 	information regarding copyright ownership
+ * 	
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ * 	
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ * 	
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ */
+package org.universaal.tools.modelling.servicemodel.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EDataType;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+import org.universaal.tools.modelling.servicemodel.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ServiceModelFactoryImpl extends EFactoryImpl implements ServiceModelFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static ServiceModelFactory init() {
+		try {
+			ServiceModelFactory theServiceModelFactory = (ServiceModelFactory)EPackage.Registry.INSTANCE.getEFactory("http://www.universaal.org/tools/modelling/servicemodel"); 
+			if (theServiceModelFactory != null) {
+				return theServiceModelFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new ServiceModelFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ServiceModelFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case ServiceModelPackage.SERVICE_INTERFACE: return createServiceInterface();
+			case ServiceModelPackage.SERVICE_OPERATION: return createServiceOperation();
+			case ServiceModelPackage.PARAMETER: return createParameter();
+			case ServiceModelPackage.SERVICE_EFFECT: return createServiceEffect();
+			case ServiceModelPackage.NAMED_ELEMENT: return createNamedElement();
+			case ServiceModelPackage.PROPERTY_PATH: return createPropertyPath();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object createFromString(EDataType eDataType, String initialValue) {
+		switch (eDataType.getClassifierID()) {
+			case ServiceModelPackage.EFFECT_TYPE:
+				return createEffectTypeFromString(eDataType, initialValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String convertToString(EDataType eDataType, Object instanceValue) {
+		switch (eDataType.getClassifierID()) {
+			case ServiceModelPackage.EFFECT_TYPE:
+				return convertEffectTypeToString(eDataType, instanceValue);
+			default:
+				throw new IllegalArgumentException("The datatype '" + eDataType.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ServiceInterface createServiceInterface() {
+		ServiceInterfaceImpl serviceInterface = new ServiceInterfaceImpl();
+		return serviceInterface;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ServiceOperation createServiceOperation() {
+		ServiceOperationImpl serviceOperation = new ServiceOperationImpl();
+		return serviceOperation;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Parameter createParameter() {
+		ParameterImpl parameter = new ParameterImpl();
+		parameter.setPropertyPath(createPropertyPath());
+		return parameter;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ServiceEffect createServiceEffect() {
+		ServiceEffectImpl serviceEffect = new ServiceEffectImpl();
+		return serviceEffect;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NamedElement createNamedElement() {
+		NamedElementImpl namedElement = new NamedElementImpl();
+		return namedElement;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PropertyPath createPropertyPath() {
+		PropertyPathImpl propertyPath = new PropertyPathImpl();
+		return propertyPath;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EffectType createEffectTypeFromString(EDataType eDataType, String initialValue) {
+		EffectType result = EffectType.get(initialValue);
+		if (result == null) throw new IllegalArgumentException("The value '" + initialValue + "' is not a valid enumerator of '" + eDataType.getName() + "'");
+		return result;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String convertEffectTypeToString(EDataType eDataType, Object instanceValue) {
+		return instanceValue == null ? null : instanceValue.toString();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ServiceModelPackage getServiceModelPackage() {
+		return (ServiceModelPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static ServiceModelPackage getPackage() {
+		return ServiceModelPackage.eINSTANCE;
+	}
+
+} //ServiceModelFactoryImpl
