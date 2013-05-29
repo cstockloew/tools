e675ffb27eea54e4b053321b70a11f4f350179de
Erlend Stav <erlend.stav@sintef.no>
2013-05-29 14:49:43 +0000

    Tagged version for the D3.1-D external revised version, including version 1.0.0 of some tools, and a 0.9.6 release candidate for the others.

---
+++ org.universaal.tools.dashboard/src/org/universaal/tools/dashboard/views/DashboardView.java
@@ -0,0 +1,195 @@
+/*
+	Copyright 2011 SINTEF, http://www.sintef.no
+
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	  http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universaal.tools.dashboard.views;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.universaal.tools.dashboard.buttonlisteners.TemporaryListener;
+import org.universaal.tools.dashboard.buttonlisteners.TemporaryMouseListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.BuildProjectListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.CreateNewItemListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.CreateNewProjectListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.CreateOntologyProjectListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.DebugProjectListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.GenerateXmlListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.ImportExampleListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.ImportThirdPartyListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.PublishProjectListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.RunProjectListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.TransformListener;
+import org.universaal.tools.dashboard.buttonlisteners.old.UploadOpenSourceListener;
+import org.universaal.tools.dashboard.listeners.ProjectElementSelectionListener;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+
+/**
+ * The Dashboardview itself.
+ * @author Adrian
+ *
+ */
+public class DashboardView extends ViewPart {
+
+	public static final String ID = "org.universaal.tools.dashboard.views.DashboardView"; //$NON-NLS-1$
+
+	private IProject project;
+
+	private ISelectionListener selectionListener;
+	private TabFolder tabFolder;
+	private Label lblProject;
+
+	public DashboardView() {
+	}
+
+	/**
+	 * Create contents of the view part.
+	 * @param parent
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		parent.setLayout(new GridLayout(1, false));
+		
+		lblProject = new Label(parent, SWT.NONE);
+		lblProject.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		lblProject.setText("Project");
+
+		ScrolledComposite scrolledComposite = new ScrolledComposite(parent, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		scrolledComposite.setExpandHorizontal(true);
+		scrolledComposite.setExpandVertical(true);
+		{
+			tabFolder = new TabFolder(scrolledComposite, SWT.NONE);
+			
+			TabItem tbtmCreatingAnAal = new TabItem(tabFolder, SWT.NONE);
+			tbtmCreatingAnAal.setText("Creating an AAL Service");
+			
+			ApplicationView applicationView = new ApplicationView(tabFolder, SWT.NONE);
+			applicationView.createActions(this);
+			tbtmCreatingAnAal.setControl(applicationView);
+			
+			TabItem tbtmOntologyProject = new TabItem(tabFolder, SWT.NONE);
+			tbtmOntologyProject.setText("Creating an Ontology");
+			
+			OntologyView ontologyView = new OntologyView(tabFolder, SWT.NONE);
+			ontologyView.createActions(this);
+			tbtmOntologyProject.setControl(ontologyView);			
+		}
+		scrolledComposite.setContent(tabFolder);
+		scrolledComposite.setMinSize(tabFolder.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+
+		initializeToolBar();
+		initializeMenu();
+		hookPageSelection();
+
+	}
+
+	/**
+	 * Initialize the toolbar.
+	 */
+	private void initializeToolBar() {
+		IToolBarManager toolbarManager = getViewSite().getActionBars()
+				.getToolBarManager();
+	}
+
+	/**
+	 * Initialize the menu.
+	 */
+	private void initializeMenu() {
+		IMenuManager menuManager = getViewSite().getActionBars()
+				.getMenuManager();
+	}
+
+	@Override
+	public void setFocus() {
+		// Set the focus
+	}
+
+	@Override
+	public void dispose(){
+		super.dispose();
+		ResourceManager.dispose();
+		SWTResourceManager.dispose();
+		if(selectionListener!=null){
+			getSite().getPage().removePostSelectionListener(selectionListener);
+		}
+	}
+
+	/**
+	 * Add selectionListener for updating Project Name
+	 */
+	private void hookPageSelection(){
+		selectionListener = new ProjectElementSelectionListener(this);
+		getSite().getPage().addPostSelectionListener(selectionListener);
+	}
+
+	private IWorkbenchPart partOfLastSelection;
+	
+	
+	public void setSelectedElementWithContext(Object selectedElement, String selectedElementString, IProject selectedProject, IWorkbenchPart partOfLastSelection) {		
+		setLastSelectionPart(partOfLastSelection);
+		setSelectedProject(selectedProject);
+		setSelectedElement(selectedElement);
+		setSelectedElementName(selectedElementString);
+	}
+	
+	
+	private void setLastSelectionPart(IWorkbenchPart partOfLastSelection) {
+		this.partOfLastSelection = partOfLastSelection;
+	}
+	
+
+	private void setSelectedElement(Object element) {
+		setSelectedElementName(element.toString());
+	}
+	
+	private void setSelectedElementName(String name){
+		if (name != null)
+			lblProject.setText(name);
+		else 
+			lblProject.setText("No element selected");			
+	}
+
+	private void setSelectedProject(IProject input){
+		this.project = input;
+	}
+
+	public IProject getSelectedProject(){
+		return project;
+	}
+	
+	public IWorkbenchPart getPartOfLastSelection() {
+		return partOfLastSelection;
+	}
+}
---
+++ org.universaal.tools.dashboard/src/org/universaal/tools/dashboard/views/OntologyView.java
@@ -0,0 +1,196 @@
+package org.universaal.tools.dashboard.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.wb.swt.ResourceManager;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.universaal.tools.dashboard.buttonlisteners.CommandCallingListener;
+import org.universaal.tools.dashboard.buttonlisteners.TemporaryListener;
+
+public class OntologyView extends Composite {
+
+	Button btnCreate;
+	Button btnImportExample;
+	Button btnTransform;
+	Button btnCreateClass;
+	Button btnBuild;
+	Button btnTestConformance;
+	Button btnCombine;
+	Button btnPublishOpenSource;
+	Button btnCreateAndEdit;
+	
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public OntologyView(Composite parent, int style) {
+		super(parent, style);
+		setLayout(new GridLayout(7, false));
+		
+		Group grpProject = new Group(this, SWT.NONE);
+		grpProject.setFont(SWTResourceManager.getFont("Lucida Grande", 11, SWT.BOLD));
+		grpProject.setBackground(SWTResourceManager.getColor(135, 206, 250));
+		grpProject.setLayout(new FillLayout(SWT.VERTICAL));
+		grpProject.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2));
+		grpProject.setText("Project");
+		
+		btnCreate = new Button(grpProject, SWT.NONE);
+		btnCreate.setText("Create");
+				
+		btnImportExample = new Button(grpProject, SWT.NONE);
+		btnImportExample.setText("Import Example");
+		
+		Composite composite = new Composite(this, SWT.NONE);
+		RowLayout rl_composite = new RowLayout(SWT.VERTICAL);
+		rl_composite.center = true;
+		composite.setLayout(rl_composite);
+		composite.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2));
+		
+		Label lblAbc = new Label(composite, SWT.NONE);
+		lblAbc.setImage(ResourceManager.getPluginImage("org.universaal.tools.dashboard", "icons/next.png"));
+		
+		btnTransform = new Button(composite, SWT.NONE);
+		btnTransform.setText("Transform");
+		
+		Group grpJavaClasses = new Group(this, SWT.NONE);
+		grpJavaClasses.setFont(SWTResourceManager.getFont("Lucida Grande", 11, SWT.BOLD));
+		grpJavaClasses.setBackground(SWTResourceManager.getColor(135, 206, 250));
+		grpJavaClasses.setLayout(new FillLayout(SWT.VERTICAL));
+		GridData gd_grpJavaClasses = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2);
+		gd_grpJavaClasses.widthHint = 108;
+		grpJavaClasses.setLayoutData(gd_grpJavaClasses);
+		grpJavaClasses.setText("Java Classes");
+		
+		btnCreateClass = new Button(grpJavaClasses, SWT.NONE);
+		btnCreateClass.setEnabled(false);
+		btnCreateClass.setText("View and Edit");
+		
+		Composite composite_1 = new Composite(this, SWT.NONE);
+		RowLayout rl_composite_1 = new RowLayout(SWT.VERTICAL);
+		rl_composite_1.center = true;
+		composite_1.setLayout(rl_composite_1);
+		composite_1.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2));
+		
+		Label label = new Label(composite_1, SWT.NONE);
+		label.setImage(ResourceManager.getPluginImage("org.universaal.tools.dashboard", "icons/next.png"));
+		
+		btnBuild = new Button(composite_1, SWT.NONE);
+		btnBuild.setText("Build");
+		
+		Group grpApplicationBinary = new Group(this, SWT.NONE);
+		grpApplicationBinary.setFont(SWTResourceManager.getFont("Lucida Grande", 11, SWT.BOLD));
+		grpApplicationBinary.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		grpApplicationBinary.setBackground(SWTResourceManager.getColor(135, 206, 250));
+		grpApplicationBinary.setText("Ontology Binary");
+		grpApplicationBinary.setLayout(new FillLayout(SWT.VERTICAL));
+		
+		btnTestConformance = new Button(grpApplicationBinary, SWT.NONE);
+		btnTestConformance.setEnabled(true);
+		btnTestConformance.setText("Test Conformance");
+		
+		Composite composite_2 = new Composite(this, SWT.NONE);
+		RowLayout rl_composite_2 = new RowLayout(SWT.VERTICAL);
+		rl_composite_2.center = true;
+		composite_2.setLayout(rl_composite_2);
+		composite_2.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2));
+		
+		Label label_1 = new Label(composite_2, SWT.NONE);
+		label_1.setImage(ResourceManager.getPluginImage("org.universaal.tools.dashboard", "icons/next.png"));
+		
+		btnCombine = new Button(composite_2, SWT.NONE);
+		btnCombine.setEnabled(false);
+		btnCombine.setText("Package");
+		
+		Group grpPublishableOntology = new Group(this, SWT.BORDER);
+		grpPublishableOntology.setFont(SWTResourceManager.getFont("Lucida Grande", 11, SWT.BOLD));
+		grpPublishableOntology.setBackground(SWTResourceManager.getColor(135, 206, 250));
+		grpPublishableOntology.setLayout(new FillLayout(SWT.VERTICAL));
+		grpPublishableOntology.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2));
+		grpPublishableOntology.setText("Publishable Ontology");
+		
+		btnPublishOpenSource = new Button(grpPublishableOntology, SWT.NONE);
+		btnPublishOpenSource.setText("Publish Open Source");
+		
+		Group grpApplicationDescription = new Group(this, SWT.NONE);
+		grpApplicationDescription.setFont(SWTResourceManager.getFont("Lucida Grande", 11, SWT.BOLD));
+		grpApplicationDescription.setBackground(SWTResourceManager.getColor(135, 206, 250));
+		GridData gd_grpApplicationDescription = new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1);
+		gd_grpApplicationDescription.widthHint = 152;
+		grpApplicationDescription.setLayoutData(gd_grpApplicationDescription);
+		grpApplicationDescription.setText("Application Description");
+		grpApplicationDescription.setLayout(new FillLayout(SWT.VERTICAL));
+		
+		btnCreateAndEdit = new Button(grpApplicationDescription, SWT.NONE);
+		btnCreateAndEdit.setText("Create and Edit");
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+	
+	DashboardView containingPart;
+	
+	private void addCommandCallingListener(Button btn, String commandID, String featureName) {
+		btn.addSelectionListener(new CommandCallingListener(containingPart, commandID, featureName));		
+	}
+	
+	/**
+	 * Use this method to create all the buttonlisteners, and assign them to 
+	 * their respective buttons.
+	 */
+	protected void createActions(DashboardView containingPart) {
+		this.containingPart = containingPart;
+		//btnImportExample.addSelectionListener(new ImportExampleListener(this));
+		addCommandCallingListener(btnImportExample,"org.universaal.importexternalproject.commands.importexample", "AAL Studio integration with Developer Depot");
+
+		//Java Classes
+		//btnCreateClass.addSelectionListener(new CreateNewItemListener(this));
+		addCommandCallingListener(btnCreateClass,"org.universaal.tools.newwizard.plugin.command.startNewItemWizard", "AAL Studio Project Wizards");
+//		btnImportClass.addSelectionListener(new TemporaryListener(this, "Import Class"));
+
+		//Application Binary
+		addCommandCallingListener(btnTestConformance, "org.universaal.tools.conformanceTools.commands.ConformanceToolsRun", "AAL Studio Conformance Tools");
+		//		btnTestConformance.addSelectionListener(new TemporaryListener(this, "Test Conformance"));
+		//btnRun.addSelectionListener(new RunProjectListener(this));
+		//addCommandCallingListener(btnRun,"org.universaal.tools.buildserviceapplication.actions.RunAction", "AAL Studio Build");
+		//btnDebug.addSelectionListener(new DebugProjectListener(this));
+		//addCommandCallingListener(btnDebug,"org.universaal.tools.buildserviceapplication.actions.DebugAction", "AAL Studio Build");
+
+		//Project Description
+		addCommandCallingListener(btnCreateAndEdit,"org.universaal.tools.uploadopensourceplugin.generateaalapp", "AAL Studio integration with Developer Depot");
+
+		//Publishable Application
+		//btnUploadOpenSource.addSelectionListener(new UploadOpenSourceListener(this));
+		addCommandCallingListener(btnPublishOpenSource,"org.universaal.tools.uploadopensourceplugin.commands.uploadopensource", "AAL Studio integration with Developer Depot");
+		//btnPublishUstore.addSelectionListener(new PublishProjectListener(this));
+		//addCommandCallingListener(btnPublishToUStore,"org.universaal.tools.buildserviceapplication.actions.PublishAction", "AAL Studio Build");
+
+		//Application Design
+		//btnCreate.addSelectionListener(new CreateOntologyProjectListener(this));
+		addCommandCallingListener(btnCreate,"org.universaal.tools.modelling.ontology.wizard.commands.newOntologyProject", "AAL Studio Modelling Support");
+		
+		// Transitions
+		addCommandCallingListener(btnBuild,"org.universaal.tools.buildserviceapplication.actions.BuildAction", "AAL Studio Build");
+		addCommandCallingListener(btnTransform,"org.universaal.tools.transformationcommand.commands.ontUML2Java", "AAL Studio Transformations");
+		
+		
+		//
+		//lblBuild.addMouseListener(new BuildProjectListener(this));
+		//lblCombine.addMouseListener(new TemporaryMouseListener(this, "Combine Project"));
+		//lblCombineArrow.addMouseListener(new TemporaryMouseListener(this, "Combine Project"));
+		//lblBuildArrow.addMouseListener(new BuildProjectListener(this));
+		//lblTransformArrow.addMouseListener(new TransformListener(this));
+		//lblTransform.addMouseListener(new TransformListener(this));
+	}	
+}
