f452bea4cc8dc23579d187e8a4b536e1fe7bf12c
Erlend Stav <erlend.stav@sintef.no>
2013-05-29 14:49:56 +0000

    Tagged version for the D3.1-D external revised version, including version 1.0.0 of some tools, and a 0.9.6 release candidate for the others.

---
+++ org.universaal.tools.modelling.service.editor/src/org/universaal/tools/modelling/servicemodel/presentation/PropertyPathDialog.java
@@ -0,0 +1,61 @@
+package org.universaal.tools.modelling.servicemodel.presentation;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+
+public class PropertyPathDialog extends Dialog {
+
+	/**
+	 * Create the dialog.
+	 * @param parentShell
+	 */
+	public PropertyPathDialog(Shell parentShell) {
+		super(parentShell);
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		
+		Label lblSelectPropertyPath = new Label(container, SWT.NONE);
+		lblSelectPropertyPath.setText("Select property path:");
+		
+		Tree tree = new Tree(container, SWT.BORDER);
+		tree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+		return container;
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
+				true);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 300);
+	}
+
+}
