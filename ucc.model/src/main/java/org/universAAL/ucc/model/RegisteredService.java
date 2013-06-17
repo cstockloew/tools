8bdd820de2c7a6267ded9ce0c096f1ee1cd4c5df
Nicole Merkle <merkle@fzi.de>
2013-06-17 22:47:42 +0000

    Added a registerd service representation

---
+++ ucc.model/src/main/java/org/universAAL/ucc/model/RegisteredService.java
@@ -0,0 +1,35 @@
+package org.universAAL.ucc.model;
+
+public class RegisteredService {
+	private String serviceId;
+	private String bundleId;
+	private String appId;
+	private String bundleVersion;
+	public String getServiceId() {
+		return serviceId;
+	}
+	public void setServiceId(String serviceId) {
+		this.serviceId = serviceId;
+	}
+	public String getBundleId() {
+		return bundleId;
+	}
+	public void setBundleId(String bundleId) {
+		this.bundleId = bundleId;
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	public String getBundleVersion() {
+		return bundleVersion;
+	}
+	public void setBundleVersion(String bundleVersion) {
+		this.bundleVersion = bundleVersion;
+	}
+	
+	
+
+}
