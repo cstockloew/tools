351060f00de6385dd07feedae50c3853101aa7ba
Erlend Stav <erlend.stav@sintef.no>
2013-09-19 13:38:57 +0000

    This tag is for the 2.0.0 release version of AAL Studio, supporting version 2.0 of the universAAL middleware.

---
+++ org.universAAL.tools.logmonitor/src/main/java/org/universAAL/tools/logmonitor/MainGui.java
@@ -0,0 +1,36 @@
+/*
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+ */
+package org.universAAL.tools.logmonitor;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class MainGui extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    private JTabbedPane tabbedPane;
+
+    /**
+     * Create the main frame.
+     */
+    public MainGui() {
+	super("Log Monitor");
+
+	tabbedPane = new JTabbedPane();
+	getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+	setSize(1000, 700);
+	setVisible(true);
+    }
+
+    public void addPanel(String title, JPanel panel) {
+	// System.out.println("Logmonitor: Adding Panel " + title);
+	tabbedPane.addTab(title, panel);
+    }
+}
---
+++ org.universAAL.tools.logmonitor/src/main/java/org/universAAL/tools/logmonitor/all_log/LogMonitor.java
@@ -0,0 +1,54 @@
+/*
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+ */
+package org.universAAL.tools.logmonitor.all_log;
+
+import java.util.Hashtable;
+
+import javax.swing.JPanel;
+import org.universAAL.tools.logmonitor.LogListenerEx;
+import org.universAAL.tools.logmonitor.all_log.gui.AllLogPanel;
+
+/**
+ * Implementation of the {@link org.universAAL.middleware.util.LogListener}
+ * interface to be called from
+ * {@link org.universAAL.middleware.container.utils.LogUtils}
+ * 
+ * @author cstockloew
+ * 
+ */
+public class LogMonitor implements LogListenerEx {
+
+    private Hashtable<Integer, LogEntry> entries = new Hashtable<Integer, LogEntry>();
+    int num = 0;
+    AllLogPanel panel;
+
+    public LogMonitor() {
+	panel = new AllLogPanel(this);
+    }
+
+    /**
+     * @see org.universAAL.middleware.container.LogListener
+     */
+    public void log(int logLevel, String module, String pkg, String cls,
+	    String method, Object[] msgPart, Throwable t) {
+
+	LogEntry le = new LogEntry(logLevel, module, pkg, cls, method, msgPart,
+		t);
+	entries.put(Integer.valueOf(num++), le);
+	panel.addMessage(le);
+    }
+
+    public JPanel getPanel() {
+	return panel;
+    }
+
+    public String getTitle() {
+	return "All Messages";
+    }
+
+    public LogEntry getLogEntry(int index) {
+	return entries.get(Integer.valueOf(index));
+    }
+}
