a834388893d8b8c00cfaa6326596c8ff0a09b737
Erlend Stav <erlend.stav@sintef.no>
2013-09-19 13:38:45 +0000

    This tag is for the 2.0.0 release version of AAL Studio, supporting version 2.0 of the universAAL middleware.

---
+++ org.universaal.tools.wizardplugin/files/1.0.0/ISubscriber.java
@@ -0,0 +1,31 @@
+/*TAG:PACKAGE*/
+
+/* More on how to use this class at: 
+ * http://forge.universaal.org/wiki/support:Developer_Handbook_8 */
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.input.InputEvent;
+import org.universAAL.middleware.input.InputSubscriber;
+
+public class /*TAG:CLASSNAME*/ extends InputSubscriber {
+
+    protected /*TAG:CLASSNAME*/(ModuleContext context) {
+	super(context);
+	// TODO Auto-generated constructor stub
+    }
+
+    public void communicationChannelBroken() {
+	// TODO Auto-generated method stub
+
+    }
+
+    public void dialogAborted(String dialogID) {
+	// TODO Auto-generated method stub
+
+    }
+
+    public void handleInputEvent(InputEvent event) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
