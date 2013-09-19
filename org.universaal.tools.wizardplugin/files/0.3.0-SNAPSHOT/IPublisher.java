11e4c930ad68f6e3e1a8b15cc18de93f5ca4e2c2
Erlend Stav <erlend.stav@sintef.no>
2013-09-19 13:38:45 +0000

    This tag is for the 2.0.0 release version of AAL Studio, supporting version 2.0 of the universAAL middleware.

---
+++ org.universaal.tools.wizardplugin/files/0.3.0-SNAPSHOT/IPublisher.java
@@ -0,0 +1,20 @@
+/*TAG:PACKAGE*/
+
+/* More on how to use this class at: 
+ * http://forge.universaal.org/wiki/support:Developer_Handbook_8 */
+import org.osgi.framework.BundleContext;
+import org.universAAL.middleware.input.InputPublisher;
+
+public class /*TAG:CLASSNAME*/ extends InputPublisher{
+
+    protected /*TAG:CLASSNAME*/(BundleContext context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+    }
+
+    public void communicationChannelBroken() {
+		// TODO Auto-generated method stub
+
+    }
+
+}
