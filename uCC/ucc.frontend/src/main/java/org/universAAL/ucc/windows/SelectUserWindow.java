73a6181b315a4f2f8628fc0f5417ae2c160d88f2
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-10-28 13:57:06 +0000

    added missing dependency, modified maven-bundle-plugin config

--- uCC/ucc.frontend/src/main/java/org/universAAL/ucc/windows/SelectUserWindow.java
+++ uCC/ucc.frontend/src/main/java/org/universAAL/ucc/windows/SelectUserWindow.java
@@ -3,9 +3,20 @@ package org.universAAL.ucc.windows;
 import java.util.List;
 import java.util.Map;
 
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.ServiceRequest;
+import org.universAAL.middleware.service.ServiceResponse;
 import org.universAAL.middleware.util.Constants;
+import org.universAAL.ontology.profile.Profilable;
+import org.universAAL.ontology.profile.Profile;
 import org.universAAL.ontology.profile.User;
+import org.universAAL.ontology.profile.service.ProfilingService;
+import org.universAAL.ontology.profile.ui.mainmenu.MenuEntry;
+import org.universAAL.ontology.profile.ui.mainmenu.MenuProfile;
 import org.universAAL.ucc.model.AALService;
+import org.universAAL.ucc.service.manager.Activator;
 
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
@@ -77,33 +88,32 @@ public class SelectUserWindow extends Window implements Button.ClickListener {
 	}
 	
 	//Adds a MenuEntry for new installed AAL service to Endusers view
-//	private void addEntry(String userID, String entryName, String vendor, String serviceClass, String iconURL)
-//    {
-//        MenuEntry me = new MenuEntry((new StringBuilder()).append(Constants.uAAL_MIDDLEWARE_LOCAL_ID_PREFIX).append(/*"nutritonalEntry"*/ entryName).toString());
-//        me.setVendor(new Resource(vendor));
-//        me.setServiceClass(new Resource(serviceClass));
-//        Resource pathElem = new Resource(iconURL);
-//        pathElem.setResourceLabel(entryName);
-//        me.setPath(new Resource[] {
-//            pathElem
-//        });
-//        ServiceRequest sr = new ServiceRequest(new ProfilingService(), null);
-//        sr.addValueFilter(new String[] {
-//            "http://ontology.universAAL.org/Profile.owl#controls"
-//        }, new User(userID));
-//        sr.addAddEffect(new String[] {
-//            "http://ontology.universAAL.org/Profile.owl#controls", "http://ontology.universAAL.org/Profile.owl#hasProfile", "http://ontology.universAAL.org/Profile.owl#hasSubProfile", "http://ontology.universaal.org/UIMainMenuProfile.owl#hasEntry"
-//        }, me);
-//        ServiceResponse res = Activator.getSc().call(sr);
-//        if(res.getCallStatus() == CallStatus.succeeded)
-//            LogUtils.logDebug(Activator.getmContext(), Activator.class, "addEntry", new Object[] {
-//                "new user ", userID, " added."
-//            }, null);
-//        else
-//            LogUtils.logDebug(Activator.getmContext(), Activator.class, "addEntry", new Object[] {
-//                "callstatus is not succeeded"
-//            }, null);
-//    }
+	private void addEntry(String userID, String entryName, String vendor, String serviceClass, String iconURL)
+    {
+        MenuEntry me = new MenuEntry((new StringBuilder()).append(Constants.uAAL_MIDDLEWARE_LOCAL_ID_PREFIX).append(/*"nutritonalEntry"*/ entryName).toString());
+        me.setVendor(new Resource(vendor));
+        me.setServiceClass(new Resource(serviceClass));
+        Resource pathElem = new Resource(iconURL);
+        pathElem.setResourceLabel(entryName);
+        me.setPath(new Resource[] {
+            pathElem
+        });
+        ServiceRequest sr = new ServiceRequest(new ProfilingService(), null);
+	sr.addValueFilter(new String[] { ProfilingService.PROP_CONTROLS },
+		new User(userID));
+	sr.addAddEffect(new String[] { ProfilingService.PROP_CONTROLS,
+		Profilable.PROP_HAS_PROFILE, Profile.PROP_HAS_SUB_PROFILE,
+		MenuProfile.PROP_ENTRY }, me);
+        ServiceResponse res = Activator.getSc().call(sr);
+        if(res.getCallStatus() == CallStatus.succeeded)
+            LogUtils.logDebug(Activator.getmContext(), Activator.class, "addEntry", new Object[] {
+                "new user ", userID, " added."
+            }, null);
+        else
+            LogUtils.logDebug(Activator.getmContext(), Activator.class, "addEntry", new Object[] {
+                "callstatus is not succeeded"
+            }, null);
+    }
 
 
 }
