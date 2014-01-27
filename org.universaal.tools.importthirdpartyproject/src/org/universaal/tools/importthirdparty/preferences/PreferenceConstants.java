c76322ba914201968bffc6e76d71f62e9da1ec36
Erlend Stav <erlend.stav@sintef.no>
2014-01-27 10:30:07 +0000

    The 3.0.0 release of the AAL Studio source and update site

---
+++ org.universaal.tools.importthirdpartyproject/src/org/universaal/tools/importthirdparty/preferences/PreferenceConstants.java
@@ -0,0 +1,7 @@
+package org.universaal.tools.importthirdparty.preferences;
+
+public class PreferenceConstants {
+
+	public static final String P_THRD_PARTY_URL = "third_party_url_preference";
+	public static final String P_EXAMPLES_URL = "examples_url_preference";
+}
