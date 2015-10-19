874cc6ca49779a06e3e82eee007b6cc78e8409af
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2015-10-19 19:12:44 +0200

    copied some parts of build plugin to create rundir

--- envsetup/org.universaal.tools.envsetup.core/src/org/universaal/tools/envsetup/core/Startup.java
+++ envsetup/org.universaal.tools.envsetup.core/src/org/universaal/tools/envsetup/core/Startup.java
@@ -19,17 +19,84 @@
  */
 package org.universaal.tools.envsetup.core;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.IStartup;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 
 /**
  * 
  * @author Carsten Stockloew
  *
- */public class Startup implements IStartup {
+ */
+public class Startup implements IStartup {
 
 	@Override
 	public void earlyStartup() {
 		// just a dummy to get the activator started
-		//System.out.println(" -- earlyStartup");
+		// System.out.println(" -- earlyStartup");
+
+		createRundir();
+		createConsole();
+	}
+
+	private void createConsole() {
+		// redirect output to default console
+		MessageConsole console = new MessageConsole("My Console", null);
+		console.activate();
+		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { console });
+		MessageConsoleStream stream = console.newMessageStream();
+		System.setOut(new PrintStream(stream));
+		System.setErr(new PrintStream(stream));
+	}
+
+	private void createRundir() {
+
+		URL url = Startup.class.getResource("files/");
+		if (url == null) {
+			// error - missing folder
+		} else {
+			File dir;
+			try {
+				dir = new File(url.toURI());
+				for (File nextFile : dir.listFiles()) {
+					System.out.println(" -- file: " + nextFile.toString());
+				}
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}
+
+		if (true)
+			return;
+		String workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString();
+		File keyDir = new File(workspacePath + File.separator + "rundir" + File.separator + "confadmin" + File.separator
+				+ "mw.bus.model.osgi");
+		keyDir.mkdirs();
+		File keyFile = new File(keyDir + File.separator + "sodapop.key");
+		if (!keyFile.exists()) {
+			try {
+				InputStream inputStream = getClass().getResourceAsStream("/files/sodapop.key");
+				OutputStream out = new FileOutputStream(keyFile);
+				byte buf[] = new byte[1024];
+				int len;
+				while ((len = inputStream.read(buf)) > 0)
+					out.write(buf, 0, len);
+				out.close();
+				inputStream.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 }
